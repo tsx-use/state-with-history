9149cca120de8a38737390b68e5c78dc52d5b913
Mark Manley <mark.manley3@icloud.com>
2024-12-10 21:19:17 -0500

    Add sanity test to make sure test runner works in CI, etc.

---
+++ index.test.ts
@@ -0,0 +1,8 @@
+import { expect, suite, test } from "vitest";
+import { useStatesTimeline } from ".";
+
+suite("useStatesTimeline", () => {
+  test("sanity test", () => {
+    expect(true).toBe(true);
+  });
+});
